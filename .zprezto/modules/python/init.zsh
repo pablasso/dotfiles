fc90125ffa47d5fcf6f78db8ba4fe3bd58f759df
Juan Pablo Ortiz <me@pablasso.com>
2017-04-17 01:11:28 -0500

    I don't want the python module, but I do want the functions.
    Hence why I didn't just disable the entire module. This is really dirty
    I need to figure out later how to better keep track of zprezto configs.

--- .zprezto/modules/python/init.zsh
+++ .zprezto/modules/python/init.zsh
@@ -7,46 +7,46 @@
 #
 
 # Load manually installed pyenv into the shell session.
-if [[ -s "$HOME/.pyenv/bin/pyenv" ]]; then
-  path=("$HOME/.pyenv/bin" $path)
-  eval "$(pyenv init -)"
+#if [[ -s "$HOME/.pyenv/bin/pyenv" ]]; then
+  #path=("$HOME/.pyenv/bin" $path)
+  #eval "$(pyenv init -)"
 
-# Load package manager installed pyenv into the shell session.
-elif (( $+commands[pyenv] )); then
-  eval "$(pyenv init -)"
+## Load package manager installed pyenv into the shell session.
+#elif (( $+commands[pyenv] )); then
+  #eval "$(pyenv init -)"
 
-# Prepend PEP 370 per user site packages directory, which defaults to
-# ~/Library/Python on Mac OS X and ~/.local elsewhere, to PATH. The
-# path can be overridden using PYTHONUSERBASE.
-else
-  if [[ -n "$PYTHONUSERBASE" ]]; then
-    path=($PYTHONUSERBASE/bin $path)
-  elif [[ "$OSTYPE" == darwin* ]]; then
-    path=($HOME/Library/Python/*/bin(N) $path)
-  else
-    # This is subject to change.
-    path=($HOME/.local/bin $path)
-  fi
-fi
+## Prepend PEP 370 per user site packages directory, which defaults to
+## ~/Library/Python on Mac OS X and ~/.local elsewhere, to PATH. The
+## path can be overridden using PYTHONUSERBASE.
+#else
+  #if [[ -n "$PYTHONUSERBASE" ]]; then
+    #path=($PYTHONUSERBASE/bin $path)
+  #elif [[ "$OSTYPE" == darwin* ]]; then
+    #path=($HOME/Library/Python/*/bin(N) $path)
+  #else
+    ## This is subject to change.
+    #path=($HOME/.local/bin $path)
+  #fi
+#fi
 
-# Return if requirements are not found.
-if (( ! $+commands[python] && ! $+commands[pyenv] )); then
-  return 1
-fi
+## Return if requirements are not found.
+#if (( ! $+commands[python] && ! $+commands[pyenv] )); then
+  #return 1
+#fi
 
-# Load virtualenvwrapper into the shell session.
-if (( $+commands[virtualenvwrapper.sh] )); then
-  # Set the directory where virtual environments are stored.
-  export WORKON_HOME="$HOME/.virtualenvs"
+## Load virtualenvwrapper into the shell session.
+#if (( $+commands[virtualenvwrapper.sh] )); then
+  ## Set the directory where virtual environments are stored.
+  #export WORKON_HOME="$HOME/.virtualenvs"
 
-  # Disable the virtualenv prompt.
-  VIRTUAL_ENV_DISABLE_PROMPT=1
+  ## Disable the virtualenv prompt.
+  #VIRTUAL_ENV_DISABLE_PROMPT=1
 
-  source "$commands[virtualenvwrapper.sh]"
-fi
+  #source "$commands[virtualenvwrapper.sh]"
+#fi
 
-#
-# Aliases
-#
+##
+## Aliases
+##
 
-alias py='python'
+#alias py='python'
